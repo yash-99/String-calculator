6389d06b71ce9cbbff40c5ba31254f1f4e3380db
Yash Patel <yashapatel3@gmail.com>
2020-06-20 23:12:28 +0530

    4th point

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,22 +1,28 @@
 package com.example.calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
 	public static int add(String numbers) {
 
 		int sum = 0;
 
-		if (numbers.contains(",") || numbers.length()>0) {
+		if (numbers.length() > 0 && numbers.startsWith("//")) {
 
-			String[] num_arr = numbers.split(",");
+			Pattern pattern=Pattern.compile("//(.)\n(.*)");
+			Matcher matcher=pattern.matcher(numbers);
+			matcher.matches();
+			String group2=matcher.group(2);
+			String[] num_arr = group2.split(";"); //";|\n"
 			for (int i = 0; i < num_arr.length; i++) {
-				sum=sum+Integer.parseInt(num_arr[i]);
+				sum = sum + Integer.parseInt(num_arr[i]);
 			}
-			
+
 		}
 
 		return sum;
 	}
-
 
 }
--- src/com/test/calculator/TestCalculator.java
+++ src/com/test/calculator/TestCalculator.java
@@ -10,19 +10,26 @@ public class TestCalculator {
 
 	@Test
 	public void testAdd() {
-		new Calculator();
 		assertEquals(3, Calculator.add("1,2"));
 	}
 	
 	@Test
 	public void testAdd2() {
-		new Calculator();
 		assertEquals(1, Calculator.add("1"));
 	}
 	
 	@Test
 	public void testAddMultipleNumbers() {
-		new Calculator();
 		assertEquals(9, Calculator.add("2,3,4"));
+	}
+	
+	@Test
+	public void testNewLineNumbers() {
+		assertEquals(12, Calculator.add("3\n5,4"));
+	}
+	
+	@Test
+	public void testDiffDelimeters() {
+		assertEquals(4, Calculator.add("//;\n2;2"));
 	}
 }
