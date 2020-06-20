def2d11ee59bc8c64af6502bd1b7e10464ead8b0
Yash Patel <yashapatel3@gmail.com>
2020-06-20 23:32:49 +0530

    point 5

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,23 +1,40 @@
 package com.example.calculator;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Calculator {
+public class Calculator extends Exception {
 
-	public static int add(String numbers) {
+	public static int add(String numbers) throws Exception {
 
 		int sum = 0;
 
 		if (numbers.length() > 0 && numbers.startsWith("//")) {
 
-			Pattern pattern=Pattern.compile("//(.)\n(.*)");
-			Matcher matcher=pattern.matcher(numbers);
+			Pattern pattern = Pattern.compile("//(.)\n(.*)");
+			Matcher matcher = pattern.matcher(numbers);
 			matcher.matches();
-			String group2=matcher.group(2);
-			String[] num_arr = group2.split(";"); //";|\n"
-			for (int i = 0; i < num_arr.length; i++) {
-				sum = sum + Integer.parseInt(num_arr[i]);
+
+			String group2 = matcher.group(2);
+
+			String[] str_arr = group2.split(";"); // ";|\n"
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			try {
+				for (int i = 0; i < str_arr.length; i++) {
+					int num = Integer.parseInt(str_arr[i]);
+					if (num < 0) {
+						list.add(num);
+					} else {
+						sum = sum + num;
+					}
+				}
+				
+				if(list.size()>0)
+				throw new Exception("negatives not allowed " + list);
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println(e.getMessage());
 			}
 
 		}
--- src/com/test/calculator/TestCalculator.java
+++ src/com/test/calculator/TestCalculator.java
@@ -9,27 +9,32 @@ import com.example.calculator.Calculator;
 public class TestCalculator {
 
 	@Test
-	public void testAdd() {
+	public void testAdd() throws Exception {
 		assertEquals(3, Calculator.add("1,2"));
 	}
 	
 	@Test
-	public void testAdd2() {
+	public void testAdd2() throws Exception {
 		assertEquals(1, Calculator.add("1"));
 	}
 	
 	@Test
-	public void testAddMultipleNumbers() {
+	public void testAddMultipleNumbers() throws Exception {
 		assertEquals(9, Calculator.add("2,3,4"));
 	}
 	
 	@Test
-	public void testNewLineNumbers() {
+	public void testNewLineNumbers() throws Exception {
 		assertEquals(12, Calculator.add("3\n5,4"));
 	}
 	
 	@Test
-	public void testDiffDelimeters() {
+	public void testDiffDelimeters() throws Exception {
 		assertEquals(4, Calculator.add("//;\n2;2"));
+	}
+	
+	@Test
+	public void testNegatives() throws Exception {
+		assertEquals(0, Calculator.add("//;\n-1;-2"));
 	}
 }
