5982d6ad1306be4a66f23876567835d0ef8c2144
Yash Patel <yashapatel3@gmail.com>
2020-06-20 19:21:11 +0530

    2nd point impl

--- src/com/test/calculator/TestCalculator.java
+++ src/com/test/calculator/TestCalculator.java
@@ -10,16 +10,19 @@ public class TestCalculator {
 
 	@Test
 	public void testAdd() {
-		assertEquals(3, new Calculator().add("1,2"));
+		new Calculator();
+		assertEquals(3, Calculator.add("1,2"));
 	}
 	
 	@Test
 	public void testAdd2() {
-		assertEquals(1, new Calculator().add("1"));
+		new Calculator();
+		assertEquals(1, Calculator.add("1"));
 	}
 	
 	@Test
-	public void testAdd3() {
-		assertEquals(0, new Calculator().add(""));
+	public void testAddMultipleNumbers() {
+		new Calculator();
+		assertEquals(9, Calculator.add("2,3,4"));
 	}
 }
